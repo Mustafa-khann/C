b526c293480c89135021be57ed49ba6a4654c854
Mustafa-khann <mustafakhann050@gmail.com>
2024-08-21 11:59:15 +0500

    Binary Search

--- cs50/binarySearch.c
+++ cs50/binarySearch.c
@@ -1,47 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
-
-int main()
-{
-    int* array = (int*)malloc(sizeof(int) * 100);
+int main() {
     int target;
+    int array[100]; // Fixed array size
 
-    int left = 0, right = 100;
-    int mid = (left + right)/2;
-
-    printf("Enter target value between the range of 0 - 100 : ");
+    printf("Enter target value between the range of 0 - 100: ");
     scanf("%i", &target);
 
-    for(int i = 0; i<100; i++){
+    if (target < 0 || target > 100) {
+        printf("Target out of bounds!\n");
+        return 1; // Indicate an error
+    }
+
+    // Create a sorted array (0 to 99)
+    for (int i = 0; i < 100; i++) {
         array[i] = i;
     }
 
-    if(target < 0 || target > 100)
-    {
-        printf("Target out of bounds!\n");
-    }
-    else
-    {    bool found = false;
+    // Binary search logic within the main function
+    int left = 0, right = 99;
+    int index = -1; // Initialize index to -1 (not found)
 
-        while(left <= right)
-        {
-            mid = (right + left ) / 2;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
 
-            if(target < mid)
-            {
-                right = mid - 1;
-            }
-            else if (target > mid)
-            {
-                left = mid + 1;
-            }
-            else if(array[mid] == target)
-            {
-                printf("Target found at index %d\n", mid);
-                break;
-            }
+        if (array[mid] == target) {
+            index = mid; // Target found
+            break; // Exit the loop
+        } else if (array[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
+
+    if (index != -1) {
+        printf("Target found at index %d\n", index);
+    } else {
+        printf("Target not found.\n");
+    }
+
+    return 0; // Indicate successful execution
 }
